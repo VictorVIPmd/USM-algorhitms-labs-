959d5474b785bb60084087ed8aeceaf540213d85
imnotvictor_tcaci <starmusicworldvip@gmail.com>
2025-04-16 11:40:40 +0300

    add labs

---
+++ Lab2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+
+class Finder
+{
+    public static int SearchLinear(int[] data, int value)
+    {
+        for (int idx = 0; idx < data.Length; idx++)
+        {
+            if (data[idx] == value)
+                return idx;
+        }
+        return -1;
+    }
+
+    public static int SearchBinary(int[] input, int value)
+    {
+        int start = 0, end = input.Length - 1;
+
+        while (start <= end)
+        {
+            int center = start + (end - start) / 2;
+
+            if (input[center] == value)
+                return center;
+            if (input[center] < value)
+                start = center + 1;
+            else
+                end = center - 1;
+        }
+
+        return -1;
+    }
+
+    public static int SearchInterpolation(int[] dataset, int targetVal)
+    {
+        int begin = 0, finish = dataset.Length - 1;
+
+        while (begin <= finish && targetVal >= dataset[begin] && targetVal <= dataset[finish])
+        {
+            if (begin == finish)
+                return dataset[begin] == targetVal ? begin : -1;
+
+            int guess = begin + ((targetVal - dataset[begin]) * (finish - begin)) / (dataset[finish] - dataset[begin]);
+
+            if (dataset[guess] == targetVal)
+                return guess;
+            if (dataset[guess] < targetVal)
+                begin = guess + 1;
+            else
+                finish = guess - 1;
+        }
+
+        return -1;
+    }
+
+    static void Main()
+    {
+        int[] lengths = { 100, 1000, 10000 };
+        int attempts = 5;
+        Random generator = new Random();
+
+        foreach (int length in lengths)
+        {
+            int[] ascArray = GenerateSequential(length);
+            int[] descArray = GenerateDescending(length);
+            int[] mixedArray = GenerateRandom(length, generator);
+
+            Evaluate("Линейный поиск", ascArray, mixedArray, attempts, SearchLinear);
+            Evaluate("Бинарный поиск", ascArray, ascArray, attempts, SearchBinary);
+            Evaluate("Интерполяционный поиск", ascArray, ascArray, attempts, SearchInterpolation);
+        }
+    }
+
+    static void Evaluate(string label, int[] searchBase, int[] testPool, int runs, Func<int[], int, int> strategy)
+    {
+        Stopwatch timer = new Stopwatch();
+        long sumTicks = 0;
+        Random rnd = new Random();
+
+        for (int run = 0; run < runs; run++)
+        {
+            int value = testPool[rnd.Next(testPool.Length)];
+            timer.Restart();
+            strategy(searchBase, value);
+            timer.Stop();
+            sumTicks += timer.ElapsedTicks;
+        }
+
+        Console.WriteLine($"{label} на массиве из {searchBase.Length} элементов: {sumTicks / runs} тиков (среднее из {runs} запусков)");
+    }
+
+    static int[] GenerateSequential(int count)
+    {
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++)
+            arr[i] = i;
+        return arr;
+    }
+
+    static int[] GenerateDescending(int count)
+    {
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++)
+            arr[i] = count - i - 1;
+        return arr;
+    }
+
+    static int[] GenerateRandom(int count, Random rnd)
+    {
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++)
+            arr[i] = rnd.Next(count * 2);
+        return arr;
+    }
+}
---
+++ Lab3.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Diagnostics;
+
+public class SingleList<T>
+{
+    public class Element
+    {
+        public T Data;
+        public Element NextElement;
+        public Element(T data) { Data = data; NextElement = null; }
+    }
+
+    public Element First { get; private set; }
+    public Element Last { get; private set; }
+    public int Size { get; private set; } = 0;
+
+    public Element AddAfter(Element reference, T data)
+    {
+        var element = new Element(data);
+        if (reference == null)
+        {
+            element.NextElement = First;
+            First = element;
+            if (Last == null) Last = element;
+        }
+        else
+        {
+            element.NextElement = reference.NextElement;
+            reference.NextElement = element;
+            if (reference == Last) Last = element;
+        }
+        Size++;
+        return element;
+    }
+
+    public (Element before, Element match) Search(T data)
+    {
+        Element previous = null, current = First;
+        while (current != null)
+        {
+            if (current.Data.Equals(data)) return (previous, current);
+            previous = current;
+            current = current.NextElement;
+        }
+        return (null, null);
+    }
+
+    public void DeleteAfter(Element reference)
+    {
+        if (reference == null)
+        {
+            if (First != null) First = First.NextElement;
+            if (First == null) Last = null;
+        }
+        else if (reference.NextElement != null)
+        {
+            if (reference.NextElement == Last) Last = reference;
+            reference.NextElement = reference.NextElement.NextElement;
+        }
+        Size--;
+    }
+
+    public void ValidateNoCycle()
+    {
+        int traversed = 0;
+        var node = First;
+        while (node != null)
+        {
+            traversed++;
+            node = node.NextElement;
+            Debug.Assert(traversed <= Size, "Обнаружен цикл в однонаправленном списке!");
+        }
+    }
+}
+
+public class DoubleList<T>
+{
+    public class Cell
+    {
+        public T Content;
+        public Cell NextCell, PrevCell;
+        public Cell(T content) { Content = content; }
+    }
+
+    public Cell Start { get; private set; }
+    public Cell End { get; private set; }
+    public int Length { get; private set; } = 0;
+
+    public Cell AppendAfter(Cell point, T content)
+    {
+        var newCell = new Cell(content);
+        if (point == null)
+        {
+            newCell.NextCell = Start;
+            if (Start != null) Start.PrevCell = newCell;
+            Start = newCell;
+            if (End == null) End = newCell;
+        }
+        else
+        {
+            newCell.NextCell = point.NextCell;
+            newCell.PrevCell = point;
+            if (point.NextCell != null) point.NextCell.PrevCell = newCell;
+            point.NextCell = newCell;
+            if (point == End) End = newCell;
+        }
+        Length++;
+        return newCell;
+    }
+
+    public Cell PrependBefore(Cell point, T content)
+    {
+        var newCell = new Cell(content);
+        if (point == null || point == Start)
+        {
+            newCell.NextCell = Start;
+            if (Start != null) Start.PrevCell = newCell;
+            Start = newCell;
+            if (End == null) End = newCell;
+        }
+        else
+        {
+            newCell.PrevCell = point.PrevCell;
+            newCell.NextCell = point;
+            point.PrevCell.NextCell = newCell;
+            point.PrevCell = newCell;
+        }
+        Length++;
+        return newCell;
+    }
+
+    public Cell Locate(T content)
+    {
+        var current = Start;
+        while (current != null)
+        {
+            if (current.Content.Equals(content)) return current;
+            current = current.NextCell;
+        }
+        return null;
+    }
+
+    public void Detach(Cell cell)
+    {
+        if (cell == null) return;
+        if (cell == Start) Start = cell.NextCell;
+        if (cell == End) End = cell.PrevCell;
+        if (cell.PrevCell != null) cell.PrevCell.NextCell = cell.NextCell;
+        if (cell.NextCell != null) cell.NextCell.PrevCell = cell.PrevCell;
+        Length--;
+    }
+
+    public void ValidateStructure()
+    {
+        int checkedCount = 0;
+        var node = Start;
+        while (node != null)
+        {
+            checkedCount++;
+            node = node.NextCell;
+            Debug.Assert(checkedCount <= Length, "Обнаружен цикл в двусвязном списке!");
+        }
+    }
+}
+
+class EntryPoint
+{
+    static void Main()
+    {
+        var list1 = new SingleList<int>();
+        var a = list1.AddAfter(null, 5);
+        var b = list1.AddAfter(a, 10);
+        list1.AddAfter(b, 15);
+        Debug.Assert(list1.Search(10).match != null);
+        list1.DeleteAfter(a);
+        list1.ValidateNoCycle();
+
+        var list2 = new DoubleList<int>();
+        var c = list2.AppendAfter(null, 100);
+        var d = list2.AppendAfter(c, 200);
+        list2.PrependBefore(d, 150);
+        Debug.Assert(list2.Locate(150) != null);
+        list2.Detach(c);
+        list2.ValidateStructure();
+
+        Console.WriteLine("Все проверки завершены успешно.");
+    }
+}
